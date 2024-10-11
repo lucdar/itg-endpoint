e477f973507f20a12e1f68e8b6bcedb442c9079c
Lucas Clark <lucas.clark@berkeley.edu>
2024-10-10 17:02:55 -0700

    proof of concept

---
+++ src/itg_buddy_endpoint/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+ROOT = Path("/mnt/c/Users/diamo/AppData/Roaming/ITGmania")
+PACKS = ROOT / "Songs"
+SINGLES = PACKS / "Singles"
+CACHE = ROOT / "Cache"
+DOWNLOADS = None
--- src/itg_buddy_endpoint/server.py
+++ src/itg_buddy_endpoint/server.py
@@ -1,9 +1,37 @@
+from concurrent import futures
 import itg_cli
-import rpc.itg_buddy_service_pb2_grpc.itg_cliServicer
+import grpc
+
+import itg_buddy_pb2_grpc
+import itg_buddy_pb2
+
+from config import SINGLES, CACHE, DOWNLOADS
+
+
+class SimfileManagementServicer(itg_buddy_pb2_grpc.SimfileManagementServicer):
+    def AddSong(self, request, context):
+        itg_cli.add_song(
+            request.path_or_url,
+            SINGLES,
+            CACHE,
+            downloads=DOWNLOADS,
+            overwrite=request.overwrite,
+            delete_macos_files_flag=False,
+        )
+        res = itg_buddy_pb2.AddSongResponse()
+        res.added_song, res.destination = "cool", "beans"
+        return res
 
 
 def main():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
+    itg_buddy_pb2_grpc.add_SimfileManagementServicer_to_server(
+        SimfileManagementServicer(), server
+    )
+    server.add_insecure_port("localhost:50051")
+    server.start()
     print("Started itg-buddy-responder!")
+    server.wait_for_termination()
 
 
 if __name__ == "__main__":
